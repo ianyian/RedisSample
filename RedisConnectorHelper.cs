c1c995455f786f7e0833613888e77b51567c9483
XJ <ianyian@gmail.com>
2021-05-30 18:26:07 +0800

    add

---
+++ RedisConnectorHelper.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redistest
+{
+    public class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("zeoRedis.redis.cache.windows.net:6380,password=HJhKU5lPmcp+pfhavegWxx7ZC+nr3Yg0c+N2UPZKwx8=,ssl=True,abortConnect=False");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
