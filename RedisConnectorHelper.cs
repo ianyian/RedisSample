bd92d0f01c5c021c771c89bab65d57afc3605f5f
XJ <ianyian@gmail.com>
2021-07-31 15:47:37 +0800

    Update RedisConnectorHelper.cs
    UPDATE

--- RedisConnectorHelper.cs
+++ RedisConnectorHelper.cs
@@ -13,7 +13,7 @@ namespace Redistest
         {
             RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
             {
-                return ConnectionMultiplexer.Connect("zeoRedis.redis.cache.windows.net:6380,password=HJhKU5lPmcp+pfhavegWxx7ZC+nr3Yg0c+N2UPZKwx8=,ssl=True,abortConnect=False");
+                return ConnectionMultiplexer.Connect("zeoRedis.redis.cache.windows.net:6380,password=HJhKU5lPmcp+pfhavegWxx7ZC+nr3Yg0c+N2UPZXXXXX,ssl=True,abortConnect=False");
             });
         }
 
